9de3623276a35c9eb3eb5426e566d6af3d63c347
stancol <stancolslick9@outlook.com>
2021-02-14 17:56:09 -0500

    Uploading logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <div>logo</div>
+        <Logo />
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Logo extends Component {
+  render() {
+    return (
+      <div className="main-logo">
+        <img src="/assets/ds_circle_logo.png" />
+      </div>
+    );
+  }
+}
+
+export default Logo;
