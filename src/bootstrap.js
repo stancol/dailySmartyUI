877249359bf307152576baec78452df7d3a1c39b
stancol <stancolslick9@outlook.com>
2021-02-15 15:18:35 -0500

    setup results component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,19 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
 
+import Home from "./components/home";
+import Results from "./components/results";
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/results" component={Results} />
+        </Switch>
       </BrowserRouter>
     </Provider>,
     document.querySelector(".app-wrapper")
