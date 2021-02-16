c621e7874cbf078a12b27f518a661f7ddfbf251e
stancol <stancolslick9@outlook.com>
2021-02-15 23:38:48 -0500

    added mapStateToProps in the recent index.js

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,19 @@
+import { SET_RECENT_POSTS } from "../actions/type";
+
+const INIT_STATE = {
+  posts: [],
+  recentPosts: [],
+};
+
+export default function (state = INIT_STATE, action) {
+  switch (action.type) {
+    case SET_RECENT_POSTS:
+      const recentPosts = action.payload;
+      return {
+        ...state,
+        recentPosts,
+      };
+    default:
+      return state;
+  }
+}
